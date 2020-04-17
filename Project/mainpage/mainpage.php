3c1ff4d89e64a759b9f27113d90ba47cad42f999
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-04-17 15:51:34 +0600

    web
    abc

---
+++ Project/mainpage/mainpage.php
@@ -0,0 +1,28 @@
+<?php
+
+include "../mainpage/common.inc.php";
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+	header("Location: ../login/login.php");
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>welcome</title>
+	<link rel="stylesheet" href="mainpage.css">
+
+</head>
+
+<body>
+</body>
+
+</html>
